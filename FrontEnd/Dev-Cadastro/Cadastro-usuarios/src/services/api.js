22c89984d69e2c7bebf11d2e9d3c9275fe0a1756
Vinícius Zanotti de Oliveira <130515950+vinizanotti89@users.noreply.github.com>
2025-04-18 00:36:34 -0300

    Update api.js

--- FrontEnd/Dev-Cadastro/Cadastro-usuarios/src/services/api.js
+++ FrontEnd/Dev-Cadastro/Cadastro-usuarios/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// Verifica se a variável de ambiente está definida, se não, usa o valor local (para desenvolvimento)
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 });
 
 export default api;
